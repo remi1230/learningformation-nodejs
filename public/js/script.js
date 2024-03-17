c2cc7eca013b3a18f7d358882835f02511b1a100
Rémi Tafforeau <remitafforeau@gmail.com>
2024-03-17 18:03:48 +0100

    Front test for user signup

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,22 @@
 const openWindow = endPoint => { window.open("http://localhost:3000/test/" + endPoint, "_blank"); };
+
+document.getElementById('signupForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    var formData = new FormData(this);
+  
+    var object = {};
+    formData.forEach((value, key) => { object[key] = value });
+    var jsonData = JSON.stringify(object);
+  
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonData
+    })
+    .then(response => response.json())
+    .then(data => console.log('Succès:', data))
+    .catch((error) => console.error('Erreur:', error));
+});
