1240d9988ad2fe459a8ed9457c4cb5a8834de2d1
Rémi Tafforeau <remitafforeau@gmail.com>
2024-03-19 18:32:34 +0100

    Create categorie vehicle

--- controllers/categorieVehicle.js
+++ controllers/categorieVehicle.js
@@ -15,11 +15,12 @@ exports.findAllCategorieVehicle = (req, res, next) => {
  };
 
 exports.createCategorieVehicle = (req, res, next) => {
-    const categorieVehicleObject = JSON.parse(req.body.categorieVehicle);
-    delete categorieVehicleObject._id;
-    const categorieVehicle = new CategorieVehicle({...categorieVehicleObject});
+    //const categorieVehicleObject = JSON.parse(req.body.categorieVehicle);
+    //delete categorieVehicleObject._id;
+    const libelle = req.body.libelle;
+    const categorieVehicle = new CategorieVehicle({libelle: libelle});
   
     categorieVehicle.save()
-    .then(() => { res.status(201).json({message: 'Véhicule enregistré !'})})
+    .then(() => { res.status(201).json({message: 'Catégorie de véhicule enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
  };
--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,84 @@
-let connectInfos = {};
+//******************************* VARIABLES GLOBALES *******************************//
+let glo = {
+  vehicles: {
+    categories: [],
+  },
+};
 
+//******************************* ÉVÈNEMENTS *******************************//
+document.addEventListener("DOMContentLoaded", function() {
+  if(this.location.pathname === '/test/vehicles'){ getAllCategorieVehicle(); }
+});
+
+//******************************* FONCTIONS *******************************//
 const openWindow = endPoint => { window.open("http://localhost:3000/test/" + endPoint, "_blank"); };
 
 function addListenerOnForm(formId, enPoint){
-  document.getElementById(formId).addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    var formData = new FormData(this);
-  
-    var object = {};
-    formData.forEach((value, key) => { object[key] = value });
-    var jsonData = JSON.stringify(object);
-  
-    fetch('http://localhost:3000/' + enPoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonData
-    })
-    .then(response => response.json())
-    .then(data => { connectInfos[formId] = data; console.log('Succès:', data)})
-    .catch((error) => console.error('Erreur:', error));
-  });
+  let form = document.getElementById(formId);
+
+  if(form){
+      document.getElementById(formId).addEventListener('submit', function(e) {
+      e.preventDefault();
+      
+      const token = localStorage.getItem('loginForm-token');
+      if((formId === 'signupForm' || formId === 'loginForm') || token){
+        var formData = new FormData(this);
+      
+        var object = {};
+        formData.forEach((value, key) => { object[key] = value });
+        var jsonData = JSON.stringify(object);
+
+        fetch('http://localhost:3000/' + enPoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: jsonData
+        })
+        .then(response => response.json())
+        .then(data => {
+          for(let prop in data){
+            localStorage.setItem(formId + '-' + prop, data[prop]);
+          }
+          console.log('Succès:', data);
+        })
+        .catch((error) => console.error('Erreur:', error));
+      }
+    });
+  }
+}
+
+function getAllCategorieVehicle(){
+  const token = localStorage.getItem('loginForm-token');
+  if(token){
+    fetch('http://localhost:3000/findAllCategorieVehicle', {
+            method: 'GET',
+            headers: {
+              'Content-Type' : 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+          })
+          .then(response => response.json())
+          .then(data => {
+            glo.vehicles.categories = data;
+
+            let categorieSelect = document.getElementById('categorie');
+
+            categorieSelect.innerHTML = '';
+            glo.vehicles.categories.forEach(categorie => {
+              let option    = document.createElement('option');
+              let optionTxt = document.createTextNode(categorie.libelle);
+
+              option.value = categorie.libelle;
+              option.appendChild(optionTxt);
+
+              categorieSelect.appendChild(option);
+            });
+            console.log('Succès:', data);
+          })
+          .catch((error) => console.error('Erreur:', error));
+    }
 }
 
 //******************************* USERS *******************************//
@@ -30,3 +86,4 @@ addListenerOnForm('signupForm', 'signup');
 addListenerOnForm('loginForm', 'login');
 
 //******************************* VEHICLES *******************************//
+addListenerOnForm('createCategorieForm', 'createCategorieVehicle');
